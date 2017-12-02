6d9a08f9d1d43a58001a6a07ac3066096d81ed91
Anton Zdanov <azdanov@protonmail.com>
2017-12-02 04:23:37 +0200

    Add cyclists.

--- src/js/index.js
+++ src/js/index.js
@@ -4,93 +4,73 @@ import data from '../cyclist-data.json';
 
 console.table(data);
 
-const w = 600;
-const h = 400;
+const w = 900;
+const h = 600;
 const padding = 40;
-const yearDays = 365;
+const offsetSeconds = 5;
 
 const parseTime = d3.timeParse('%M:%S');
-const parseYear = d3.timeParse('%Y');
 
-// For converting Dates to strings
-const formatYear = d3.timeFormat('%Y');
 const formatTime = d3.timeFormat('%M:%S');
 
-// Discover start and end dates in dataset
-const startDate = parseYear(d3.min(data, d => d.Year));
-const endDate = parseYear(d3.max(data, d => d.Year));
-
-// Discover start and end dates in dataset
 const startTime = parseTime(d3.min(data, d => d.Time));
 const endTime = parseTime(d3.max(data, d => d.Time));
 
-console.log(startDate, endDate);
-console.log(startTime, endTime);
+const eventYears = [...new Set(data.map(d => d.Year))].sort((a, b) => a - b);
 
-// Create scale functions
 const xScale = d3
-  .scaleTime()
-  .domain([
-    d3.timeDay.offset(startDate, -yearDays),
-    d3.timeDay.offset(endDate, yearDays),
-  ])
+  .scaleBand()
+  .domain(eventYears)
   .range([padding, w - padding]);
 
 const yScale = d3
   .scaleTime()
-  .domain([d3.timeDay.offset(startTime, -1), d3.timeDay.offset(endTime, 1)])
+  .domain([
+    d3.timeSecond.offset(endTime, offsetSeconds),
+    d3.timeSecond.offset(startTime, -offsetSeconds),
+  ])
   .range([h - padding, padding]);
 
-// Define X axis
-const xAxis = d3
-  .axisBottom(xScale)
-  .ticks(9)
-  .tickFormat(formatYear);
+const xAxis = d3.axisBottom(xScale);
 
-// Define Y axis
 const yAxis = d3
   .axisLeft(yScale)
   .ticks(10)
   .tickFormat(formatTime);
 
-// Create SVG element
 const svg = d3
   .select('body')
   .append('svg')
   .attr('width', w)
   .attr('height', h);
 
-// Generate guide lines
-svg
-  .selectAll('line')
-  .data(data)
-  .enter()
-  .append('line')
-  .attr('x1', d => xScale(parseYear(d.Year)))
-  .attr('x2', d => xScale(parseYear(d.Year)))
-  .attr('y1', h - padding)
-  .attr('y2', d => yScale(parseTime(d.Time)))
-  .attr('stroke', '#ddd')
-  .attr('stroke-width', 1);
-
-// Generate circles last, so they appear in front
 svg
   .selectAll('circle')
   .data(data)
   .enter()
   .append('circle')
-  .attr('cx', d => xScale(parseYear(d.Year)))
-  .attr('cy', d => yScale(parseTime(d.Time)))
-  .attr('r', 2);
+  .attr('cx', d => xScale(d.Year) + xScale.bandwidth() / 2)
+  .attr('cy', d => {
+    return yScale(parseTime(d.Time))
+  })
+  .attr('r', 5);
 
-// Create X axis
+svg
+  .selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .classed('cyclist', true)
+  .text(d => d.Name)
+  .attr('x', d => xScale(d.Year) + xScale.bandwidth() / 2 + 10)
+  .attr('y', d => yScale(parseTime(d.Time)) + 4);
+
 svg
   .append('g')
   .attr('class', 'axis')
   .attr('transform', `translate(0,${h - padding})`)
   .call(xAxis);
 
-// Create Y axis
 svg
   .append('g')
   .attr('class', 'axis')
