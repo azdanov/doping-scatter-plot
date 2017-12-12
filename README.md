05d2719d8f62cb495cda8b24b6c453940ac47f77
Anton Zdanov <akumael@gmail.com>
2017-12-12 10:03:06 +0200

    Remove background. Add footer. Update Readme.

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # Scatter plot
 
-D3.js Scatter plot made for FreeCodeCamp.
+D3.js Scatter Plot made for FreeCodeCamp.
+
+![Chart](https://res.cloudinary.com/anton-zdanov/image/upload/q_auto/v1513065709/Screen_Shot_2017-12-12_at_10.00.50_vla4uk.png)
 
 [Live version](https://azdanov.js.org/scatter-plot/)
+
+## Tools
+
+* [d3](https://github.com/d3/d3)
+* [babel](https://github.com/babel/babel)
+* [webpack](https://github.com/webpack)
+* [eslint](https://github.com/eslint/eslint)
+* [airbnb Code Style](https://github.com/airbnb/javascript)
+* [prettier](https://github.com/prettier/prettier)
+* [stylelint](https://github.com/stylelint/stylelint)
+* [cross-env](https://github.com/kentcdodds/cross-env)
+* [gh-pages](https://github.com/tschaub/gh-pages)
+* [npm-run-all](https://github.com/mysticatea/npm-run-all)
+* [rimraf](https://github.com/isaacs/rimraf)
+
+## Data
+
+Source: [Ammattipyöräily](http://www.fillarifoorumi.fi/forum/showthread.php?38129-Ammattilaispy%F6r%E4ilij%F6iden-nousutietoja-%28aika-km-h-VAM-W-W-kg-etc-%29&p=2041608#post2041608)
+
+[cyclist-data.json](https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json)
+
+## Build
+
+* `npm start` - start webpack-dev-server
+* `npm build` - build for production
+
+## License
+
+[Unlicense](https://unlicense.org)
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
-import * as d3 from 'd3';
-import '../css/index.css';
-import data from '../cyclist-data.json';
+import "../css/index.css";
+import data from "../cyclist-data.json";
 
 const w = 700;
 const h = 600;
@@ -8,114 +7,109 @@ const padding = 40;
 const offsetSeconds = 5;
 const circleRadius = 5;
 
-const parseTime = d3.timeParse('%M:%S');
+const parseTime = d3.timeParse("%M:%S");
 
-const formatTime = d3.timeFormat('%M:%S');
+const formatTime = d3.timeFormat("%M:%S");
 
-const startTime = parseTime(d3.min(data, d => d.Time));
-const endTime = parseTime(d3.max(data, d => d.Time));
+const startTime = parseTime(d3.min(data, ({ Time }) => Time));
+const endTime = parseTime(d3.max(data, ({ Time }) => Time));
 
-const eventYears = [...new Set(data.map(d => d.Year))].sort((a, b) => a - b);
+const eventYears = [...new Set(data.map(({ Year }) => Year))].sort((a, b) => a - b);
 
 const [startYear, endYear] = d3.extent(eventYears, d => d);
 
 const xScale = d3
-  .scaleBand()
-  .domain(eventYears)
-  .range([padding, w - padding]);
+    .scaleBand()
+    .domain(eventYears)
+    .range([padding, w - padding]);
 
 const yScale = d3
-  .scaleTime()
-  .domain([
-    d3.timeSecond.offset(endTime, offsetSeconds),
-    d3.timeSecond.offset(startTime, -offsetSeconds),
-  ])
-  .range([h - padding, padding]);
+    .scaleTime()
+    .domain([d3.timeSecond.offset(endTime, offsetSeconds), d3.timeSecond.offset(startTime, -offsetSeconds)])
+    .range([h - padding, padding]);
 
 const xAxis = d3.axisBottom(xScale);
 
 const yAxis = d3
-  .axisLeft(yScale)
-  .ticks(10)
-  .tickFormat(formatTime);
+    .axisLeft(yScale)
+    .ticks(10)
+    .tickFormat(formatTime);
 
-d3.select('h1').text(`Fastest times up Alpe d'Huez`);
-d3.select('p').text(`Data from year ${startYear} up to ${endYear}.`);
+d3.select("h1").text(`Fastest times up Alpe d'Huez`);
+d3.select("p").text(`Data from year ${startYear} up to ${endYear}.`);
 
 const svg = d3
-  .select('main')
-  .append('svg')
-  .attr('width', w)
-  .attr('height', h);
+    .select("main")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-const onMouseOver = d => {
-  svg
-    .selectAll('circle')
-    .classed(
-      'selected',
-      cd => `${d.Name} + ${d.Year}` === `${cd.Name} + ${cd.Year}`,
-    )
-    .attr(
-      'r',
-      cd =>
-        `${d.Name} + ${d.Year}` === `${cd.Name} + ${cd.Year}`
-          ? circleRadius + 2
-          : circleRadius,
-    );
-  d3
-    .select('.cyclist-stats')
-    .text(`${d.Name} (${d.Nationality}) - ${d.Time} - ${d.Year}`)
-    .classed('cyclist-info--visible', true);
+const onMouseOver = ({ Name, Year, Nationality, Time }) => {
+    svg
+        .selectAll("circle")
+        .classed(
+            "selected",
+            ({ Name: currentName, Year: currentYear }) => `${Name} + ${Year}` === `${currentName} + ${currentYear}`,
+        )
+        .attr(
+            "r",
+            ({ Name: currentName, Year: currentYear }) =>
+                `${Name} + ${Year}` === `${currentName} + ${currentYear}` ? circleRadius + 2 : circleRadius,
+        );
+    d3
+        .select(".cyclist-stats")
+        .text(`${Name} (${Nationality}) - ${Time} - ${Year}`)
+        .classed("cyclist-info--visible", true);
 };
 
 const onMouseOut = () => {
-  svg.selectAll('circle').classed('selected', () => {
-    svg
-      .selectAll('circle')
-      .classed('selected', () => false)
-      .attr('r', circleRadius);
-  });
-  d3.select('.cyclist-stats').classed('cyclist-info--visible', false);
+    svg.selectAll("circle").classed("selected", () => {
+        svg
+            .selectAll("circle")
+            .classed("selected", () => false)
+            .attr("r", circleRadius);
+    });
+    d3.select(".cyclist-stats").classed("cyclist-info--visible", false);
 };
 
 svg
-  .selectAll('circle')
-  .data(data)
-  .enter()
-  .append('circle')
-  .attr('cx', d => xScale(d.Year) + xScale.bandwidth() / 2)
-  .attr('cy', d => yScale(parseTime(d.Time)))
-  .attr('r', circleRadius)
-  .on('mouseover', onMouseOver)
-  .on('mouseout', onMouseOut);
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", ({ Year }) => xScale(Year) + xScale.bandwidth() / 2)
+    .attr("cy", ({ Time }) => yScale(parseTime(Time)))
+    .attr("r", circleRadius)
+    .on("mouseover", onMouseOver)
+    .on("mouseout", onMouseOut);
 
 svg
-  .append('g')
-  .attr('class', 'axis')
-  .attr('transform', `translate(0,${h - padding})`)
-  .call(xAxis);
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", `translate(0, ${h - padding})`)
+    .call(xAxis);
 
 svg
-  .append('g')
-  .attr('class', 'axis')
-  .attr('transform', `translate(${padding},0)`)
-  .call(yAxis);
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", `translate(${padding}, 0)`)
+    .call(yAxis);
 
 const infoPanel = d3
-  .select('main')
-  .append('div')
-  .style('height', `${h - padding * 1.7}px`)
-  .classed('info', true);
+    .select("main")
+    .append("div")
+    .style("height", `${h - padding * 1.7}px`)
+    .classed("info", true);
 
-infoPanel.append('h2').text(`Top ${data.length} cyclists`);
+infoPanel.append("h2").text(`Top ${data.length} cyclists`);
 
 infoPanel
-  .selectAll('p')
-  .data(data)
-  .enter()
-  .append('p')
-  .on('mouseover', onMouseOver)
-  .on('mouseout', onMouseOut)
-  .text(d => `${d.Place}. ${d.Name} (${d.Year})`)
-  .append('span')
-  .text(d => `${d.Time}`);
+    .selectAll("p")
+    .data(data)
+    .enter()
+    .append("p")
+    .on("mouseover", onMouseOver)
+    .on("mouseout", onMouseOut)
+    .text(({ Place, Name, Year }) => `${Place}. ${Name} (${Year})`)
+    .append("span")
+    .text(({ Time }) => `${Time}`);
